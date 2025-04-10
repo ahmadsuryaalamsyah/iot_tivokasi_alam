61774db9909a002605a163e30bd24262d8b6fc99
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2025-04-10 15:42:10 +0700

    fetch: add project minggu 7

---
+++ esp32_real_led Minggu 7/src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+const int lampPin = 33;  //Merah
+const int lampPin2 = 25; //Kuning
+const int lampPin3 =  32; //hijau
+
+void setup() {
+  pinMode(lampPin, OUTPUT);
+  pinMode(lampPin2, OUTPUT);
+  pinMode(lampPin3, OUTPUT);
+}
+
+void loop() {
+  Serial.println("Lampu Merah");
+  digitalWrite(lampPin, HIGH); 
+  digitalWrite(lampPin2, LOW); 
+   digitalWrite(lampPin3, LOW);
+  delay(10000);  
+
+  Serial.println("Lampu Kuning");
+  digitalWrite(lampPin, LOW);   
+  digitalWrite(lampPin2, HIGH);  
+  digitalWrite(lampPin3, LOW);
+  delay(5000);  
+
+  Serial.println("Lampu Hijau");
+  digitalWrite(lampPin, LOW);   
+  digitalWrite(lampPin2, LOW);  
+  digitalWrite(lampPin3, HIGH);
+  delay(10000);  
+}
